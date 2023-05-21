07da3d1a0f6085cef7ac87821963e225538e9206
Даниил Мухреев <muxreev.dan2@yandex.ru>
2023-05-21 18:26:07 +0300

    feat: :fire: Добавлена карточка товара

---
+++ src/assets/imgs/svgs/delete.tsx
@@ -0,0 +1,27 @@
+export const DeleteSvg = () => {
+  return (
+    <svg
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_267_434)">
+        <path
+          d="M14 8.28571H9.71429H8.28571H4V9.71428H8.28571H9.71429H14V8.28571Z"
+          fill="#3C270D"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_267_434">
+          <rect
+            width="18"
+            height="18"
+            fill="white"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
