4acfe779705c7792d20cf5c69f5fb32398920571
Даниил Мухреев <muxreev.dan2@yandex.ru>
2023-05-21 19:37:28 +0300

    feat: :ambulance: Добавлен select

--- src/components/ui/input/index.tsx
+++ src/components/ui/input/index.tsx
@@ -1,15 +1,24 @@
-import { Input } from 'antd';
+import { Input, Select } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 
 import styles from './input.module.scss';
 import clsx from 'clsx';
-import { IInputSearch } from './types';
+import { IInputSearch, IInputSelect } from './types';
 
 export const SearchInput: React.FC<IInputSearch> = ({ ...probs }) => {
   return (
     <Input
       suffix={<SearchOutlined />}
-      className={clsx(styles.input)}
+      className={clsx(styles.input, probs.className)}
+      {...probs}
+    />
+  );
+};
+
+export const SelectInput: React.FC<IInputSelect> = ({ ...probs }) => {
+  return (
+    <Select
+      className={clsx(styles.select, probs.className)}
       {...probs}
     />
   );
--- src/components/ui/input/types.ts
+++ src/components/ui/input/types.ts
@@ -1,3 +1,6 @@
+import { SelectProps } from 'antd';
 import { SearchProps } from 'antd/es/input';
 
 export interface IInputSearch extends SearchProps {}
+
+export interface IInputSelect extends SelectProps {}
