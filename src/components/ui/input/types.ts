4736a56d2553713566acd083ac7c99cce7c362b2
Даниил Мухреев <muxreev.dan2@yandex.ru>
2023-05-21 19:47:28 +0300

    feat: :memo: Добавлен Slider Input

--- src/components/ui/input/types.ts
+++ src/components/ui/input/types.ts
@@ -1,6 +1,9 @@
 import { SelectProps } from 'antd';
 import { SearchProps } from 'antd/es/input';
+import { SliderRangeProps } from 'antd/es/slider';
 
 export interface IInputSearch extends SearchProps {}
 
 export interface IInputSelect extends SelectProps {}
+
+export interface IInputSlider extends SliderRangeProps {}
