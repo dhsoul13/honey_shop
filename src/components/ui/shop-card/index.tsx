fe47230b2a901821e876d429b00a13bf2720dbae
Даниил Мухреев <muxreev.dan2@yandex.ru>
2023-05-22 23:33:08 +0300

    style: :fire: Изменены стили в карт элементе

--- src/components/ui/shop-card/index.tsx
+++ src/components/ui/shop-card/index.tsx
@@ -1,32 +1,24 @@
 import { IShopCard, IShopCardMap } from './types';
-
 import styles from './shopCard.module.scss';
 import { Divider } from 'antd';
 import { MainButton } from '../buttons';
-import { useState } from 'react';
-import { BasketSvg } from 'src/assets/imgs/svgs/basket';
-import { AddingSvg } from 'src/assets/imgs/svgs/adding';
-import { DeleteSvg } from 'src/assets/imgs/svgs/delete';
+import { UseCount } from 'src/utils/hooks/useCount';
+import {
+  MinusOutlined,
+  PlusOutlined,
+  ShoppingOutlined,
+} from '@ant-design/icons';
+import clsx from 'clsx';
 
 export const ShopCard: React.FC<IShopCard> = ({ content }) => {
-  const [count, setCount] = useState(1);
-
-  const increaseByOneOnClick = () => {
-    setCount((prev) => prev + 1);
-  };
-
-  const reduceByOneOnClick = () => {
-    setCount((prev) => {
-      if (count === 1) {
-        return 1;
-      } else {
-        return prev - 1;
-      }
-    });
-  };
+  const [count, addFunct, delFunct, status] = UseCount({
+    availableCount: content.count,
+  });
 
   return (
-    <div className={styles.container}>
+    <div
+      className={clsx(styles.container, status === 'not' ? styles.disable : '')}
+    >
       <div className={styles.img_block}>
         <img
           src="https://2sotki.ru/wp-content/uploads/2/7/9/279cb03a22dc0bce7cd7244f4f2eff5e.png"
@@ -42,22 +34,34 @@ export const ShopCard: React.FC<IShopCard> = ({ content }) => {
         </div>
         <div className={styles.functional__block}>
           <div className={styles.adding_functional}>
-            <div className={styles.count}>{count}</div>
             <div
-              onClick={increaseByOneOnClick}
-              className={styles.svg}
+              className={clsx(
+                styles.count,
+                status === 'not' || status === 'crowded' ? styles.disable : ''
+              )}
             >
-              <AddingSvg />
+              {count}
             </div>
-            <div
-              onClick={reduceByOneOnClick}
-              className={styles.svg}
+            <MainButton
+              type="link"
+              onClick={addFunct}
+              disabled={status === 'not'}
             >
-              <DeleteSvg />
-            </div>
+              <PlusOutlined />
+            </MainButton>
+            <MainButton
+              type="link"
+              onClick={delFunct}
+              disabled={status === 'not'}
+            >
+              <MinusOutlined />
+            </MainButton>
           </div>
-          <MainButton>
-            <BasketSvg />
+          <MainButton
+            className={styles.btn_add_basket}
+            disabled={status === 'not'}
+          >
+            <ShoppingOutlined />
           </MainButton>
         </div>
       </div>
